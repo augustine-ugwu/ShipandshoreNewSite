5557b046184540793fa5464c1e797c3a3c749d96
augustine-ugwu <augustinehills@yahoo.com>
2024-12-27 13:56:59 +0100

    Added certifications

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import ContactForm from "@/components/ContactForm";
 import Footer from "@/components/Footer";
 import About from "@/components/About";
 import Team from "@/components/Team";
+import Certification from "@/components/Certification";
 
 export default function HomePage() {
   return (
@@ -12,6 +13,7 @@ export default function HomePage() {
       <Navbar />
       <About />
       <Team />
+      <Certification />
       <ContactForm />
       <Footer />
     </div>
---
+++ src/components/Certification.js
@@ -0,0 +1,49 @@
+export default function Certification() {
+  return (
+    <section id="certifications" className="bg-white p-12">
+      <hr className="p-8" />
+      <div className="container mx-auto text-center ">
+        <h2 className="font-bebas-neue text-4xl tracking-wider text-gray-800">
+          Certified By
+        </h2>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-5 gap-4 mt-4 mx-auto max-w-5xl px-4">
+        <div className="flex justify-center items-center">
+          <img
+            src="/assets/Cert_1.png"
+            alt="Certification 1"
+            className="h-auto max-h-24"
+          />
+        </div>
+        <div className="flex justify-center items-center">
+          <img
+            src="/assets/Cert_2.png"
+            alt="Certification 2"
+            className="h-auto max-h-24"
+          />
+        </div>
+        <div className="flex justify-center items-center">
+          <img
+            src="/assets/Cert_3.png"
+            alt="Certification 3"
+            className="h-auto max-h-24"
+          />
+        </div>
+        <div className="flex justify-center items-center">
+          <img
+            src="/assets/Cert_4.png"
+            alt="Certification 4"
+            className="h-auto max-h-24"
+          />
+        </div>
+        <div className="flex justify-center items-center">
+          <img
+            src="/assets/Cert_5.png"
+            alt="Certification 5"
+            className="h-auto max-h-24"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
