a56bf0854567102855e1a6b6f0d92999f868ad06
augustine-ugwu <augustinehills@yahoo.com>
2025-01-09 07:31:51 +0100

    MOD Navbar 2

--- src/app/components/Navbar2.js
+++ src/app/components/Navbar2.js
@@ -14,11 +14,10 @@ export default function Navbar() {
 
   return (
     <header
-      className={`fixed top-0 left-0 w-full bg-black text-white z-50 transition-all duration-700 ease-out shadow-md ${
+      className={`top-0 w-full bg-black text-white z-50 transition-all duration-700 ease-out shadow-md ${
         isAnimated ? "animate-navbar" : ""
       }`}>
-      <nav className="container mx-auto flex items-center justify-between py-4 px-8">
-        {/* Logo */}
+      <nav className="container mx-auto flex items-center justify-between p-8">
         <a href="/">
           <div className="text-lg tracking-wider flex items-center space-x-2">
             <img
@@ -31,7 +30,7 @@ export default function Navbar() {
         </a>
 
         {/* Desktop Navigation */}
-        <ul className="hidden lg:flex space-x-8 text-xl">
+        <ul className="hidden lg:flex space-x-8 text-xl ">
           <li>
             <a
               href="/"
@@ -43,15 +42,13 @@ export default function Navbar() {
             className="relative group"
             onMouseEnter={() => setIsAboutDropdownOpen(true)}
             onMouseLeave={() => setIsAboutDropdownOpen(false)}>
-            <a
-              href="/about"
-              className="nav font-bebas-neue relative cursor-pointer">
+            <a className="nav font-bebas-neue relative cursor-pointer">
               About Us
               <span className="absolute left-0 bottom-0 w-0 h-1 bg-white transition-all transform duration-300"></span>
             </a>
             {/* Dropdown Menu */}
             {isAboutDropdownOpen && (
-              <ul className="absolute top-full left-0 bg-black text-white w-48 py-2 shadow-lg text-sm">
+              <ul className="z-50 absolute top-full left-0 bg-black text-white w-48 py-2 shadow-lg text-sm">
                 <li className="px-4 py-2 hover:bg-gray-800">
                   <a href="/management">Management</a>
                 </li>
@@ -77,6 +74,24 @@ export default function Navbar() {
               className="nav font-bebas-neue relative cursor-pointer">
               Contact
             </a>
+          </li>
+          <li>
+            <button
+              className="flex items-center justify-center text-black transition-all hover:opacity-80 transform duration-200 pl-6"
+              aria-label="Search">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="24"
+                height="24"
+                viewBox="0 0 50 50"
+                className="w-6 h-6">
+                <path
+                  fill="#000000"
+                  d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
+              </svg>
+            </button>
           </li>
         </ul>
 
@@ -114,9 +129,7 @@ export default function Navbar() {
           <a href="/" className="font-bebas-neue text-2xl">
             Home
           </a>
-          <a href="/about" className="font-bebas-neue text-2xl">
-            About Us
-          </a>
+          <a className="font-bebas-neue text-2xl">About Us</a>
           <a href="/services" className="font-bebas-neue text-2xl">
             Services
           </a>
